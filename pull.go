15670f330272c406bd4a1496b3a2e53940b6d802
lichenji <1714815278@qq.com>
2020-12-13 18:02:55 +0800

    Add 修改pull

--- pull.go
+++ pull.go
@@ -4,36 +4,28 @@ import (
 	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
-	mvccpb "github.com/coreos/etcd/mvcc/mvccpb"
 )
 
-func (m *Manger) PullOne(name string, configs map[string]string) {
+func (m *Manger) PullConfig(name string) []byte {
 	var key = fmt.Sprintf("%s/%s", m.appKey, name)
-	m.pull(key, configs)
+	return m.pull(key)
 }
 
-func (m *Manger) PullKvs(names []string, configs map[string]string) {
-	for _, name := range names {
-		m.PullOne(name, configs)
-	}
+func (m *Manger) PullAppConfigs() []byte {
+	return m.pull(m.appKey)
 }
 
-func (m *Manger) PullOverAll(configs map[string]string) {
-	m.pull(m.appKey, configs, clientv3.WithPrefix())
-}
-
-func (m *Manger) pull(name string, configs map[string]string, opts ...clientv3.OpOption) {
+func (m *Manger) pull(name string, opts ...clientv3.OpOption) []byte {
 	var (
 		getResp *clientv3.GetResponse
-		kvs     *mvccpb.KeyValue
 		err     error
 	)
 	if getResp, err = m.kv.Get(context.TODO(), name, opts...); err != nil {
 		fmt.Println(err)
 	}
 
-	for _, kvs = range getResp.Kvs {
-		key := string(kvs.Key)[len(m.appKey)+1:]
-		configs[key] = string(kvs.Value)
+	if len(getResp.Kvs) == 0 {
+		fmt.Println("key: %s not found in etcd", name)
 	}
+	return getResp.Kvs[0].Value
 }
