db1cc7fa4b22cb52c49bbf6d58f1e1339825061e
lichenji <1714815278@qq.com>
2020-12-14 15:21:31 +0800

    Fixed 修改pull

--- pull.go
+++ pull.go
@@ -4,11 +4,25 @@ import (
 	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"strings"
 )
 
-func (m *Manger) PullConfig(name string) []byte {
-	var key = fmt.Sprintf("%s/%s", m.appKey, name)
+func (m *Manger) PullOne(name string) []byte {
+	var key string
+	if strings.Contains(name, "/") {
+		key = fmt.Sprintf("%s/%s", name, m.env)
+	} else {
+		key = fmt.Sprintf("/%s/%s/%s", m.app, name, m.env)
+	}
 	return m.pull(key)
+}
+
+func (m *Manger) PullKvs(names []string) []byte {
+	var buffers = make([]byte, 0)
+	for _, name := range names {
+		buffers = append(buffers, m.PullOne(name)...)
+	}
+	return buffers
 }
 
 func (m *Manger) PullAppConfigs() []byte {
@@ -20,12 +34,14 @@ func (m *Manger) pull(name string, opts ...clientv3.OpOption) []byte {
 		getResp *clientv3.GetResponse
 		err     error
 	)
+	fmt.Println("----", name)
 	if getResp, err = m.kv.Get(context.TODO(), name, opts...); err != nil {
 		fmt.Println(err)
 	}
 
 	if len(getResp.Kvs) == 0 {
-		fmt.Println("key: %s not found in etcd", name)
+		fmt.Println("not found the key: ", name)
+		return []byte{}
 	}
 	return getResp.Kvs[0].Value
 }
